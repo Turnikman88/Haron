e045aabaffaa02854299c84d3b233fbab219c2d9
Nikolay Velikov <73721605+NikolayVelikov@users.noreply.github.com>
2022-09-27 22:14:53 +0300

    Create IdentityOption.cs

---
+++ src/Haron.Application.Core.Identity/Options/IdentityOption.cs
@@ -0,0 +1,11 @@
+﻿namespace Haron.Application.Core.Identity.Options
+{
+    public class IdentityOption
+    {
+        public UserOption? User { get; set; }
+
+        public SignInOption? SignIn { get; set; }
+
+        public PasswordOption? Password { get; set; }
+    }
+}
