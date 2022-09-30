460cecd87d6789a7644f91d931c2123f62532d7a
Nikolay Velikov <73721605+NikolayVelikov@users.noreply.github.com>
2022-09-30 16:29:35 +0300

    Create UserManagerService.cs

---
+++ src/Haron.Application.Core.Identity/Services/UserManagerService.cs
@@ -0,0 +1,18 @@
+﻿using Haron.Application.Core.Identity.Options;
+
+namespace Haron.Application.Core.Identity.Services
+{
+    public class UserManagerService
+    {
+        private readonly PasswordManager _passwordManager;
+        private readonly SignInManager _signInManager;
+        private readonly UserManager _userManager;
+
+        public UserManagerService(IdentityOption identityOption)
+        {
+            _passwordManager = new PasswordManager(identityOption.PasswordRequrments);
+            _signInManager = new SignInManager(identityOption.SignInRequrments);
+            _userManager = new UserManager(identityOption.UserRequrments);
+        }
+    }
+}
