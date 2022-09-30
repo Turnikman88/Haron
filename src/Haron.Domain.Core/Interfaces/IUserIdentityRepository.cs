79ec45e13c483c3fc07bd6cd608d9b419ee64a05
Nikolay Velikov <73721605+NikolayVelikov@users.noreply.github.com>
2022-09-30 16:29:38 +0300

    Create IUserIdentityRepository.cs

---
+++ src/Haron.Domain.Core/Interfaces/IUserIdentityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Haron.Domain.Core.Entities;
+
+namespace Haron.Domain.Core.Interfaces
+{
+    public interface IUserIdentityRepository
+    {
+        Task<User> GetByAsync(int id);
+
+        Task<User> GetByAsync(string value, bool email = true);
+
+        Task<bool> EmailOrUsernameExist(string value, bool email = true);
+
+        Task<bool> EmailAndUsernameExist(string value);
+    }
+}
